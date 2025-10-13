78bf29a229c877216b3eff1e8a9e1db9c3f3db04
jjy2kk3 <jjy2kk3@gmail.com>
2025-10-12 20:26:49 -0700

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 export default function Logo({ className = "" }: { className?: string }) {
   return (
     <div className={`flex items-end gap-2 ${className}`}>
-      {/* 左侧圆形 LM 徽标 */}
+      {/* 圆形 LM 徽标 */}
       <div
         className="w-8 h-8 rounded-full bg-black grid place-items-center flex-shrink-0"
         aria-label="LM"
@@ -14,12 +14,13 @@ export default function Logo({ className = "" }: { className?: string }) {
         </span>
       </div>
 
-      {/* 右侧文字：Layer Moon + Home Staging */}
+      {/* 文字区：Layer Moon + Home Staging */}
       <div className="flex flex-col leading-none">
         <span className="font-semibold text-[18px] tracking-tight">
           Layer Moon
         </span>
-        <span className="text-[12px] text-neutral-500 relative -top-0.5 left-[1px]">
+        {/* 注意：向右 +2px，向下 +2px */}
+        <span className="text-[12px] text-neutral-500 relative top-[2px] ml-[2px]">
           Home&nbsp;Staging
         </span>
       </div>
